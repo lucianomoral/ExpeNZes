1feef7ab73911abf15d4c8b81516aa9de60e2333
lucianomoral <lucianof.moral@gmail.com>
2020-07-23 11:39:53 +1200

    Configuraciones para correr en Heroku (Gunicorn, requirements, etc)

--- ExpeNZes/settings.py
+++ ExpeNZes/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
-django_heroku.setting(locals())
+django_heroku.settings(locals())
